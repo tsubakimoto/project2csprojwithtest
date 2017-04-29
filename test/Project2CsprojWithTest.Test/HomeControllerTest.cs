8c0601f52a5b456446326d800318f3371ed7e523
tsubakimoto <tkmbmbr@gmail.com>
2017-04-29 22:31:33 +0900

    Setup unit test.

---
+++ test/Project2CsprojWithTest.Test/HomeControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project2CsprojWithTest.Controllers;
+using Xunit;
+
+namespace Project2CsprojWithTest.Test
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            var ctrl = new HomeController();
+            var acturl = ctrl.Index();
+            Assert.IsType<ViewResult>(acturl);
+        }
+    }
+}
